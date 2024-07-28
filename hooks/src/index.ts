34ff14a2382c2b4ac2a7f0a7b5f1bb8041a59f46
prblmsolvrx <realgoblu@gmail.com>
2024-07-28 11:49:28 +0530

    commit

--- hooks/src/index.ts
+++ hooks/src/index.ts
@@ -1,5 +1,13 @@
 import express from "express"
 
-
-
 const app = express();
+
+// https://hooks.zapier.com/hooks/catch/19600626/2udryib/
+
+app.post("hooks/catch/:userId/:zapId",(req,res)=>{
+     const userId = req.params.userId
+     const zapId = req.params.zapId
+
+     //store in DB a new trigger
+     // push in on to a queue(redis/kafka) 
+})
