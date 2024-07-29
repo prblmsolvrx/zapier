1de25a96f139cc4cc579a2b6c29a5bf8faeaf8cd
prblmsolvrx <realgoblu@gmail.com>
2024-07-29 15:00:12 +0530

    commit

--- hooks/src/index.ts
+++ hooks/src/index.ts
@@ -1,13 +1,32 @@
 import express from "express"
+import {PrismaClient} from "@prisma/client";
+
+const client = new PrismaClient();
 
 const app = express();
 
 // https://hooks.zapier.com/hooks/catch/19600626/2udryib/
 
-app.post("hooks/catch/:userId/:zapId",(req,res)=>{
+app.post("hooks/catch/:userId/:zapId",async (req,res)=>{
      const userId = req.params.userId
      const zapId = req.params.zapId
+     const body = req.body
 
      //store in DB a new trigger
-     // push in on to a queue(redis/kafka) 
+     await client.$transaction(async tx =>{
+          const run = await client.zapRun.create({
+               data: {
+                    zapId : zapId,
+                    metadata : body
+               }
+          });
+          await client.zapRunOutbox.create({
+               data: {
+                    zapRunId: run.id
+               }
+          })
+     })
+
+     // push in on to a queue(redis/kafka)
+     // this zapid needs to be executed
 })
